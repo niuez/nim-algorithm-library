7795819a155341799c1833d1d62274c3ebd77f97
Kutimotitokura <kutimoti.tokura@gmail.com>
2018-10-08 21:20:46 +0900

    change method name

--- self-BalancingBinarySearchTree/RBSTArray.nim
+++ self-BalancingBinarySearchTree/RBSTArray.nim
@@ -23,52 +23,52 @@ proc newRBSTArray*[T]() : RBSTArray[T] =
   tree.root = nil
   return tree
 
-proc node_size[T](node : RBSTNode[T]) : int =
+proc size[T](node : RBSTNode[T]) : int =
   if node == nil: return 0
   return node.sz
 
-proc node_update[T](node : var RBSTNode[T]) : RBSTNode[T] =
+proc fix[T](node : var RBSTNode[T]) : RBSTNode[T] =
   if node == nil : return nil
-  node.sz = 1 + node.left.node_size + node.right.node_size
+  node.sz = 1 + node.left.size + node.right.size
   return node
 
 
 proc merge*[T](l : var RBSTNode[T] , r : var RBSTNode[T]) : RBSTNode[T] =
   if l == nil : return r
   if r == nil : return l
-  discard node_update(l)
-  discard node_update(r)
-  if rand(rbstRand,l.node_size + r.node_size - 1) < l.node_size:
+  discard fix(l)
+  discard fix(r)
+  if rand(rbstRand,l.size + r.size - 1) < l.size:
     l.right = merge(l.right , r)
-    return node_update(l)
+    return fix(l)
   else:
     r.left = merge(l , r.left)
-    return node_update(r)
+    return fix(r)
 
 proc split*[T](node : var RBSTNode[T] , k : int) : tuple[l : RBSTNode[T],r : RBSTNode[T]] =
   if node == nil: return (nil,nil)
-  discard node_update(node)
-  if k <= node.left.node_size:
+  discard fix(node)
+  if k <= node.left.size:
     var s = split(node.left , k)
     node.left = s.r
-    return (s.l , node_update(node))
+    return (s.l , fix(node))
   else:
-    var s = split(node.right , k - node.left.node_size - 1)
+    var s = split(node.right , k - node.left.size - 1)
     node.right = s.l
-    return (node_update(node) , s.r)
+    return (fix(node) , s.r)
 
 proc insert*[T](tree : var RBSTArray[T] , k : int , val : T) =
   var s = split(tree.root , k)
   var z = newRBSTNode(val)
   tree.root = merge(s.l , z)
   tree.root = merge(tree.root , s.r)
-  discard node_update(tree.root)
+  discard fix(tree.root)
 
 proc erase*[T](tree : var RBSTArray[T] , k : int) =
   var t = split(tree.root , k + 1)
   var s = split(t.l , k)
   tree.root = merge(s.l , t.r)
-  discard node_update(tree.root)
+  discard fix(tree.root)
   
 proc merge*[T](tree1 : var RBSTArray[T] , tree2 : var RBSTArray[T]) =
   tree1.root = merge(tree1.root , tree2.root)
@@ -87,10 +87,10 @@ proc find[T](tree : RBSTArray[T] , k : int) : RBSTNode[T] =
   var n = k
   var now = tree.root
   while now != nil :
-    if now.left.node_size == n:
+    if now.left.size == n:
       return now
-    if now.left.node_size < n:
-      n -= now.left.node_size + 1
+    if now.left.size < n:
+      n -= now.left.size + 1
       now = now.right
     else:
       now = now.left
